b35b742bf63836d63f0f7ead31ad4d26913ab9b7
Saif Ali <this.saif.email@gmail.com>
2025-03-05 04:00:12 +0530

    created: types for context, middleware and route handler

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+//Type Definition and Context Interface
+
+
+import {IncomingMessage, ServerResponse} from "http";
+
+/**
+ * Context interface bundles everything like:
+ * - Request: The node.js request
+ * - Response: The node.js response
+ * - Params: The route parameters
+ * - body: parsed data from request body 
+ * - state: a place to store extra data you want to share or pass around
+ */
+
+// Context Interface
+export interface Context {
+    req: IncomingMessage;
+    res: ServerResponse;
+    params?: Record<string, string>
+    body?: any;
+    state?: Record<string, any>;
+}
+
+export type Next = () => void;
+
+/**
+ * Middleware functions receives the context(ctx) and Next function.
+ * It can be used to perform tasks before sending repsonse to client like Logging, Parsing, etc.
+ */
+export type Middleware = (ctx: Context, next: Next) => void;
+
+/**
+ * Route handler function receives the context(ctx) and should return a response.
+ */
+
+export type RouteHandler = (ctx: Context) => void;
+
