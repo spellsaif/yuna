4d3da22b8c6cdab2135ec49987ef9f814eab0595
Saif Ali <this.saif.email@gmail.com>
2025-03-06 05:50:32 +0530

    updated: reimplemented kamiJson(json parser)

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,19 +14,31 @@ export const log: Middleware = (ctx, next) => {
 /**
  * Json parser middleware to parse the request body
  */
-export const jsonParser: Middleware = (ctx, next) => {
+export const kamiJson: Middleware = (ctx, next) => {
     let data = '';
     ctx.req.on('data', chunk => {
         data += chunk.toString();
     });
 
     //after data is received completely, we need to parse it
-    try {
-        ctx.req.on('end', () => {
-            ctx.body = JSON.parse(data);
-        })
-    }catch(err) {
-        ctx.body = data;
+    const contentType = ctx.req.headers['content-type'] || '';
+
+    if(contentType.includes('application/json')) { 
+        // logging raw data for debugging
+        console.log("Received Json Body", data);
+        try {
+            // if data is empty after trimming, we will set it to empty object
+            if(data.trim() === '') {
+                ctx.body = {};
+            } else {
+                ctx.body = JSON.parse(data);
+            }
+        } catch (error) {
+            console.error("Error Parsing JSON", error);
+            ctx.body = {};
+        }
+    } else {
+        ctx.body = {};
     }
 
     next();
