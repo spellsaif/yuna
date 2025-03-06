0b2055caac6e64f97affa479e05da13366e567de
Saif Ali <this.saif.email@gmail.com>
2025-03-06 23:48:03 +0530

    updated: made nekoTrace log more beautifully

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,62 @@
 
+import chalk from "chalk";
 import { Middleware } from "./types";
 
 /**
  * Middleware to log the request method and url
 */
 
-export const nekoTrace: Middleware = (ctx, next) => {
-    console.log(`Method:${ctx.req.method}  Url: ${ctx.req.url}`);
-    next();
+// export const nekoTrace: Middleware = (ctx, next) => {
+//     console.log(`Method:${ctx.req.method}  Url: ${ctx.req.url}`);
+//     next();
+// }
+
+interface NekoTraceOptions {
+  logRequest?: boolean;
+  logResponse?: boolean;
+  customPrefix?: string;
+}
+
+const defaultOption: NekoTraceOptions = {
+  logRequest: true,
+  logResponse: true,
+  customPrefix: 'NekoTrace'
+}
+
+
+export const nekoTrace = (options: NekoTraceOptions  = {}): Middleware => {
+  const config = {...defaultOption, ...options};
+  return (ctx, next) => {
+    const prefix = config.customPrefix;
+    if (config.logRequest) {
+      console.log(`${chalk.gray(new Date().toISOString())} ${chalk.blue("[INFO]")} ✨ ${chalk.magenta(`[${prefix}]`)} Incoming request: ${ctx.req.method} ${ctx.req.url}`);
+    }
+
+    const result: any = next(); 
+
+    if (result && typeof result.then === "function") {
+      return result.then(() => {
+        if (config.logResponse) {
+          console.log(
+            `${chalk.gray(new Date().toISOString())} ${chalk.green("[SUCCESS]")} 🎉 ${chalk.magenta(
+              `[${prefix}]`
+            )} Response sent with status: ${ctx.res.statusCode}`
+          );
+        }
+      });
+    }
+
+    if (config.logResponse) {
+      console.log(
+        `${chalk.gray(new Date().toISOString())} ${chalk.green("[SUCCESS]")} 🎉 ${chalk.magenta(
+          `[${prefix}]`
+        )} Response sent with status: ${ctx.res.statusCode}`
+      );
+    }
+
+    return result;
+
+  }
 }
 
 
