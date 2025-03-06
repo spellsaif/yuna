cac7c8a46ee8d271ca8ae2d595ec17951a6453fd
Saif Ali <this.saif.email@gmail.com>
2025-03-07 03:50:40 +0530

    modified: default entry point for yuna.js

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,4 @@
-import { Context } from "./types";
-import createYuna from "./yuna";
-import { nekoTrace, kamiJson } from "./middlewares";
-
-const app = createYuna();
-
-//middlewares
-app.summon(nekoTrace());
-app.summon(kamiJson);
+export {default as Yuna} from "./yuna";
+export * from "./middlewares";
 
 
-app.get("/", (ctx:Context) => {
-    ctx.whisper({
-        message: "Hello, Yuna!"
-    });
-
-});
-
-app.tribe('/api', (api) => {
-    api.get('/users', (ctx) => ctx.whisper({ message: 'List of users' }));
-    api.post('/users', (ctx) => ctx.whisper({ message: 'User created' }));
-    api.get('/users/:id', (ctx) => ctx.whisper({ message: `User ID: ${ctx.params.id}` }));
-});
-
-app.get("/about/:name", (ctx: Context) => {
-    const {name} = ctx.params;
-    ctx.whisper(`<h1> Hi, ${name}</h1>`)
-})
-
-app.post("/login", (ctx: Context) => {
-    console.log(ctx.body);
-    ctx.whisper("object received");
-})
-
-app.serve(3000, () => {
-    console.log("Server is running on port http://localhost:3000");
-})
